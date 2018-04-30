f0cb16b53e03ac5b41a42470b7f68c002304b7fd
qf-home <qf0129@qq.com>
2018-04-30 17:43:39 +0800

    temp

--- car.py
+++ car.py
@@ -1,152 +1,76 @@
-
 import time
 from os import system
 from RPi import GPIO
 
-
-in1=33
-in2=35
-in3=37
-in4=36
-
+in1=31
+in2=33
+in3=35
+in4=37
 
 class Car(object):
-
 	def __init__(self):
 
-		self.gpio = GPIO
-		self.gpio.setmode(gpio.BOARD)
+		self.gpio = GPIO()
+		self.gpio.setmode(self.gpio.BOARD)
 		self.gpio.setwarnings(False)
+		self.running = False
 
-		for i in range(4):
-			self.gpio.setup(in1, gpio.OUT)
+		for i in range(1, 5):
+			self.gpio.setup(eval('in'+str(i)), self.gpio.OUT)
+			self.gpio.output(eval('in'+str(i)), 0)
 
+	def stop(self):
+		if self.running:
+			print('>>stop')
 			self.gpio.output(in1, 0)
+			self.gpio.output(in2, 0)
+			self.gpio.output(in3, 0)
+			self.gpio.output(in4, 0)
+			self.running = False
+
+	def up(self):
+		print('>>up')
+		self.gpio.output(in1, 1)
+		self.gpio.output(in2, 0)
+		self.gpio.output(in3, 0)
+		self.gpio.output(in4, 1)
+		self.running = True
+		
+	def down(self):
+		print('>>down')
+		self.gpio.output(in1, 0)
+		self.gpio.output(in2, 1)
+		self.gpio.output(in3, 1)
+		self.gpio.output(in4, 0)
+		self.running = True
+
+	def left(self):
+		print('>>left')
+		self.gpio.output(in1, 0)
+		self.gpio.output(in2, 1)
+		self.gpio.output(in3, 0)
+		self.gpio.output(in4, 1)
+		self.running = True
+
+	def right(self):
+		print('>>right')
+		self.gpio.output(in1, 1)
+		self.gpio.output(in2, 0)
+		self.gpio.output(in3, 1)
+		self.gpio.output(in4, 0)
+		self.running = True
 
 
-
-
-
-# -*- coding:utf-8 -*-
-
-import RPi.GPIO
-import time
-from os import system
-
-in1=33
-in2=35
-in3=37
-in4=36
-
-in5=3 #led灯
-
-gpio=RPi.GPIO
-
-power=False
-
-
-def powerON():
-	global power,gpio,in1,in2,in3,in4,in5
-	
-	gpio.setmode(gpio.BOARD)
-	gpio.setwarnings(False)
-
-	gpio.setup(in1, gpio.OUT)
-	gpio.setup(in2, gpio.OUT)
-	gpio.setup(in3, gpio.OUT)
-	gpio.setup(in4, gpio.OUT)
-	gpio.setup(in5, gpio.OUT)
-
-	gpio.output(in1, 0)
-	gpio.output(in2, 0)
-	gpio.output(in3, 0)
-	gpio.output(in4, 0)
-	gpio.output(in5, 1)
-
-	power=True
-	print '>>>>>>>>>>>>>>> PowerON'
-
-def powerOFF():
-	global power,gpio
-	gpio.cleanup()
-	power=False
-	print '>>>>>>>>>>>>>>> PowerOFF'
-
-
-
-def ledOn():
-	global power,gpio,in1,in2,in3,in4,in5
-	if not power:
-		print 'PowerOFF!'
-		return
-	gpio.output(in5, 0)
-
-def ledOff():
-	global power,gpio,in1,in2,in3,in4,in5
-	if not power:
-		print 'PowerOFF!'
-		return
-	gpio.output(in5, 1)
-
-def forward():
-	global power,gpio,in1,in2,in3,in4
-	if not power:
-		print 'PowerOFF!'
-		return
-	#print '>>forward'
-
-	gpio.output(in1, 1)
-	gpio.output(in2, 0)
-	gpio.output(in3, 0)
-	gpio.output(in4, 1)
-	
-def back():
-	global power,gpio,in1,in2,in3,in4
-	if not power:
-		print 'PowerOFF!'
-		return
-	#print '>>back'
-
-	gpio.output(in1, 0)
-	gpio.output(in2, 1)
-	gpio.output(in3, 1)
-	gpio.output(in4, 0)
-
-def left():
-	global power,gpio,in1,in2,in3,in4
-	if not power:
-		print 'PowerOFF!'
-		return
-	#print '>>left'
-
-	gpio.output(in1, 0)
-	gpio.output(in2, 1)
-	gpio.output(in3, 0)
-	gpio.output(in4, 1)
-
-def right():
-	global power,gpio,in1,in2,in3,in4
-	if not power:
-		print 'PowerOFF!'
-		return
-	#print '>>right'
-
-	gpio.output(in1, 1)
-	gpio.output(in2, 0)
-	gpio.output(in3, 1)
-	gpio.output(in4, 0)
-
-def stop():
-	global power,gpio,in1,in2,in3,in4
-	if not power:
-		print 'PowerOFF!'
-		return
-	#print '>>stop'
-
-	gpio.output(in1, 0)
-	gpio.output(in2, 0)
-	gpio.output(in3, 0)
-	gpio.output(in4, 0)
-
-def shutdown():
-	system('poweroff')
+class DebugGPIO(object):
+	def BOARD(self):
+		pass
+	def OUT(self):
+		pass
+	def setmode(self, param):
+		pass
+	def setwarnings(self, param):
+		pass
+	def setup(self, param, param2):
+		pass
+	def output(self, param, param2):
+		pass
--- web.py
+++ web.py
@@ -1,8 +1,24 @@
-from bottle import get, run, template
+from bottle import get, run, template, static_file
+from car import Car
 
+car = Car()
 
 @get('/')
 def index():
-	return template("index.html")
+	return template("tpl/index.html")
+
+@get('/static/<filename:re:.*\.*>')
+def server_static(filename):
+    return static_file(filename, root= './static')
+
+
+@get('/car/control/<command>')
+def car_control(command):
+	if not hasattr(car, command):
+		return {'code': -1, 'msg': 'can not find command'}
+	cmd = getattr(car, command)
+	cmd()
+
+	return {'code': 0}
 
 run(host='0.0.0.0', port=8001, reloader=True, debug=True)
