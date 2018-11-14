31b20532724478b144f418969ce7f9e61d03fd7d
qf <qf0129@qq.com>
2018-11-14 19:17:49 +0800

    save

--- app_test_touch.py
+++ app_test_touch.py
@@ -13,7 +13,7 @@ from geventwebsocket.handler import WebSocketHandler
 app = Bottle()
 @app.get('/')
 def index():
-    return template('tpl/index')
+    return template('tpl/test_touch')
 
 @app.route('/static/<filename>')
 def server_static(filename):
@@ -36,11 +36,11 @@ def handle_websocket():
 # def send_html(filename):
 #     return static_file(filename, root='./static', mimetype='text/html')
 
-host = "0.0.0.0"
-port = 8000
+# host = "0.0.0.0"
+# port = 8000
 
-server = WSGIServer((host, port), app, handler_class=WebSocketHandler)
-print "access @ http://%s:%s/websocket.html" % (host,port)
-server.serve_forever()
+# server = WSGIServer((host, port), app, handler_class=WebSocketHandler)
+# print("access @ http://%s:%s/websocket.html" % (host,port))
+# server.serve_forever()
 
-# app.run(host='0.0.0.0', port=8000, reloader=True, server='gevent', handler_class=WebSocketHandler)
+app.run(host='0.0.0.0', port=8000, reloader=True, server='gevent', handler_class=WebSocketHandler)
